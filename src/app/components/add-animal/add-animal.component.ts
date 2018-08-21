9f24514d2914b86c93b5f3f6c0df46e05cc3b0f5
Oscar Chilel <oskr.chilel@gmail.com>
2018-08-20 20:05:02 -0600

    add animal component

--- src/app/components/add-animal/add-animal.component.ts
+++ src/app/components/add-animal/add-animal.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimalService } from '../../services/animal.service';
+import { Animal } from '../../models/animal';
+import { Router } from '../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-add-animal',
   templateUrl: './add-animal.component.html',
-  styleUrls: ['./add-animal.component.css']
+  styleUrls: ['./add-animal.component.css'],
+  providers: [
+    AnimalService
+  ]
 })
 export class AddAnimalComponent implements OnInit {
 
-  constructor() { }
+  private animal: Animal;
+
+  constructor(
+    private animalService: AnimalService,
+    private router: Router,
+  ) {
+    this.animal = new Animal('', '', '');
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.animalService.createAnimal(this.animal)
+      .subscribe(
+        result => {
+          if (result.animal) {
+            this.router.navigate(['/animals']);
+          } else {
+            console.log(result);
+          }
+        },
+        error => {
+          alert(error._body);
+        }
+      );
   }
 
 }
